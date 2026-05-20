dd15fb7097b9529893fe591c292316576c128780
Jayasurya R <62130969+Jayasurya-Ramalingam@users.noreply.github.com>
2026-05-20 12:52:11 +0530

    Add semaphore implementation for consumer problem
    Implement a semaphore-based consumer problem with threading.

---
+++ Semaphore_Implementation.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+//semaphore declaration
+sem_t sid;
+int cnt=0;
+
+void worker_Consumer_problem(int Value)
+{
+    printf("Thread %lu is waiting\n", pthread_self());
+    //sem_wait(&sid);
+    cnt++;
+    sleep(1);
+    printf("Thread Executed Successfully:%d\n",cnt);
+    //sem_post(&sid);
+    
+}
+
+void main()
+{
+    pthread_t tid;
+    pthread_t tid2;
+
+    sem_init(&sid,0,1);
+    
+    pthread_create(&tid,NULL,(void*)worker_Consumer_problem,12);
+    pthread_create(&tid2,NULL,(void*)worker_Consumer_problem,15);
+  
+
+    //Without join it wont work
+    pthread_join(tid2,NULL);
+    pthread_join(tid,NULL);
+
+}
